764c324b07638530eedd5db2bcb38338709a5840
Manjit Guha <manjit.guha@gmail.com>
2017-01-14 22:16:10 +0530

    Changed

--- start/appointmentsvc.go
+++ start/appointmentsvc.go
@@ -85,6 +85,7 @@ func (t *SimpleChaincode) getAppointment(stub shim.ChaincodeStubInterface, args 
 
     valAsbytes, err := stub.GetState(key)
     if err != nil {
+        log.Fatalln("Failed to open log file", ":", err)
         jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
         return nil, errors.New(jsonResp)
     }
